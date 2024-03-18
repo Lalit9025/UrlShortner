d1198801a543076c88debef5fa3e069c0b91955a
Lalit9025 <lalit3chouhan@gmail.com>
2024-03-18 18:28:07 +0530

    server updated

--- server/server.js
+++ server/server.js
@@ -15,11 +15,11 @@ app.use(express.json())
 
 app.use(cors())
 
-app.use(express.static(path.join(__dirname, '../client/build')));
+// app.use(express.static(path.join(__dirname, '../client/build')));
 
-app.get('*',(req,res) => {
-    res.sendFile(path.join(__dirname + '/../client/build/index.html'));
-})
+// app.get('*',(req,res) => {
+//     res.sendFile(path.join(__dirname + '/../client/build/index.html'));
+// })
 
 //routes
 app.use('/api/v1/urls',urlRoutes)
